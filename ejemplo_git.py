a5f902de408a23c1cfebd59f33a08b89fd8846ab
walterblasquezcontreras <wa.blasquez@duocuc.cl>
2024-05-29 09:23:45 -0400

    cree mi diccionario

--- ejemplo_git.py
+++ ejemplo_git.py
@@ -1,1 +1,8 @@
 #ejercicio diccionario de hoy: usuario:
+diccionario = {
+    "identificador": 1,
+    "nombre": "marriott",
+    "direccion": "avenida bernardo O'higgins 1123",
+    "funcionando": True,
+    "Valor_Habitacion": 100000
+}
